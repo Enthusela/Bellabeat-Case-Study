ade61dda311bdfe3734551c1311f54b0b41799ec
Enthusela <112834651+Enthusela@users.noreply.github.com>
2023-12-05 17:00:38 +0800

    Added intensity/distance analysis
    Concluded that the cohort does engage in outdoor activities, based on a strong correlation between active minutes and active distance.

--- Bellabeat_BGJ.R
+++ Bellabeat_BGJ.R
@@ -1,46 +1,42 @@
+wide_to_stacked_bar_plot <- function(data_wide, key, value, key_order) {
+  if(!("id" %in% colnames(data_wide))) {
+    print("ERROR: data does not include \"id\" column: cannot convert.")
+  } else {
+    # Convert the data from wide to long. Set the factor levels to control the stacking order of the bars
+    data_long <- data_wide %>%
+      tidyr::gather(key = !!sym(key), value = !!sym(value), -id) %>%
+      mutate(!!sym(key) := factor(!!sym(key), levels = key_order))
 
-ggplot_histo_pareto <- function (data, bin_width) {
-  # Cumulative data uses same bins as histogram
-  data <- sort(data)
-  rounding_factor <- 1 / bin_width
-  data_rounded <- round(data * rounding_factor) / rounding_factor
+    # Order IDs in wide data based on value of first key, then rearrange long data.
+    # This ensures the resultant plot sorts the IDs in ascending order of the first key
+    first_key <- key_order[1]
+    data_wide <- data_wide %>% arrange(!!sym(first_key))
+    data_long$id <- factor(
+      data_long$id,
+      levels = data_wide$id)
 
-  # Calculate highest count so axes can be adjusted
-  highest_count <- max(table(data_rounded))
-  data_pareto = seq(1, length(data), by = 1) / length(data) * highest_count
-
-  # Calculate stats for overlay on histogram
-  data_max <- max(data)
-  data_mean <- mean(data)
-  data_median <- median(data)
-
-  p <- ggplot() +
-    geom_histogram(aes(x = data),
-                   color = "white",
-                   binwidth = bin_width) +
-    scale_x_continuous(breaks = seq(0, data_max + bin_width, by = bin_width)) +
-    geom_line(aes(x = data_rounded,
-                  y = data_pareto,
-                  color="Cumulative")) +
-    geom_vline(aes(xintercept = data_mean,
-                   color = "Mean")) +
-    geom_vline(aes(xintercept = data_median,
-                   color = "Median")) +
-    scale_y_continuous(name = "Count",
-                       breaks = seq(0, highest_count, by = 1),
-                       sec.axis = sec_axis(~./highest_count, name = "Cumulative Percentage of Count")) +
-    scale_color_manual(name="Stats",
-                       breaks = c("Cumulative", "Mean", "Median"),
-                       values = c("Cumulative" = "red",
-                                  "Mean" = "green",
-                                  "Median" = "blue")) +
-    labs(x = "Value")
-  return(p)
+    p <- ggplot(data_long, aes(x= id, y= !!sym(value), fill= !!sym(key))) +
+      geom_bar(stat = "identity") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    # scale_y_continuous(breaks = seq(0, 24, by = 1))
+  }
 }
 
-walking_time_averaged <- walking_times %>%
-  group_by(id) %>%
-  summarize(average_mod_brisk = mean(moderate_walking + brisk_walking))
+data <- distance_vs_intensity %>%
+  select(id,
+         pct_distance_very_active,
+         pct_distance_moderately_active,
+         pct_distance_lightly_active,
+         pct_distance_sedentary)
 
-plot_mod_brisk <- ggplot_histo_pareto(walking_time_averaged$average_mod_brisk, 0.25)
-print(plot_mod_brisk)
+stack_order <- c("pct_distance_very_active",
+                 "pct_distance_moderately_active",
+                 "pct_distance_lightly_active",
+                 "pct_distance_sedentary")
+
+p <- wide_to_stacked_bar_plot(data_wide = data,
+                              key = "intensity",
+                              value = "pct_distance",
+                              key_order = stack_order)
+
+print(p)
