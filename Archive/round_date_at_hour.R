cc80cfae945abc8f97dd6b0ed7a6f3d3a3a7705e
Enthusela <112834651+Enthusela@users.noreply.github.com>
2023-12-23 14:34:40 +0800

    Completed the case study.
    All unneeded files moved to Archive: Archive was then removed from the repo onto my personal OneDrive for storage. All other files relocated to sub-directories: the main directory now only includes the R project files, and the final HTML file.

--- Archive/round_date_at_hour.R
+++
@@ -1,8 +0,0 @@
-round_date_at_hour <- function(dt, hr) {
-  timestamp <- ifelse (
-    hour(dt) >= hr,
-    ceiling_date(dt, unit = "days"),
-    floor_date(dt, unit = "days")
-  )
-  return(as_datetime(timestamp, tz = "UTC"))
-}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Bellabeat-Case-Study
  
+This is the data analysis case study I completed as part of the Google Data Analytics Professional Certificate.
+
+The final case study itself is included in "BellabeatCaseStudy.html".
